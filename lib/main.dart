bd9d0582dcd4f7e0333fec1619e8490f807588c1
bacnk <nguyenkhacbac1233@gmail.com>
2023-05-31 14:37:02 +0700

    new camera screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,36 @@
-import 'package:exercise/widgets/home_screen.dart';
+import 'package:camera/camera.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MaterialApp(
-    home: HomeScreen(),
-  ));
+import 'screens/camera_screen.dart';
+
+List<CameraDescription> cameras = [];
+
+Future<void> main() async {
+  // Fetch the available cameras before initializing the app.
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
+    cameras = await availableCameras();
+  } on CameraException catch (e) {
+    if (kDebugMode) {
+      print('Error in fetching the cameras: $e');
+    }
+  }
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: CameraScreen(),
+    );
+  }
 }
